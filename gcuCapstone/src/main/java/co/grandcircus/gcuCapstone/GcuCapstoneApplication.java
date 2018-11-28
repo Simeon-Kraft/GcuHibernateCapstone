c60b3bfeaa259330653bf5f5ee1d5b7efa30e7e1
Simeon Kraft <kraft.simeon.sk@gmail.com>
2018-11-28 09:31:58 -0500

    the template

---
+++ gcuCapstone/src/main/java/co/grandcircus/gcuCapstone/GcuCapstoneApplication.java
@@ -0,0 +1,12 @@
+package co.grandcircus.gcuCapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcuCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcuCapstoneApplication.class, args);
+	}
+}
---
+++ gcuCapstone/src/main/java/co/grandcircus/gcuCapstone/GcuCapstoneController.java
@@ -0,0 +1,5 @@
+package co.grandcircus.gcuCapstone;
+
+public class GcuCapstoneController {
+
+}
