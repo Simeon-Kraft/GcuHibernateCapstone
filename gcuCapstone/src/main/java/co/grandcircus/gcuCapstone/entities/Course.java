7873e58df5f5c757bde0fb2d353c43c301b80d06
Simeon Kraft <kraft.simeon.sk@gmail.com>
2018-11-28 11:03:23 -0500

    enrollments fields

--- gcuCapstone/src/main/java/co/grandcircus/gcuCapstone/entities/Course.java
+++ gcuCapstone/src/main/java/co/grandcircus/gcuCapstone/entities/Course.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -18,8 +19,42 @@ public class Course {
 	private Long id;
 	private String name;
 	private String category;
+	@OneToMany
 	private List<Enrollments> list;
 	
 	public Course() {}
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public List<Enrollments> getList() {
+		return list;
+	}
+
+	public void setList(List<Enrollments> list) {
+		this.list = list;
+	}
+	
+	
 }
--- gcuCapstone/src/main/java/co/grandcircus/gcuCapstone/entities/Enrollments.java
+++ gcuCapstone/src/main/java/co/grandcircus/gcuCapstone/entities/Enrollments.java
@@ -1,5 +1,24 @@
 package co.grandcircus.gcuCapstone.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "enrollments")
 public class Enrollments {
+	
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private Student student;
+	@ManyToOne
+	private Course course;
+	private Double grade;
+	private String semester;
+	
+	public Enrollments() {}
 
 }
