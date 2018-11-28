1359c1762280e274696e40d59f013086db74c187
Simeon Kraft <kraft.simeon.sk@gmail.com>
2018-11-28 11:41:58 -0500

    added getters/setters

--- gcuCapstone/src/main/java/co/grandcircus/gcuCapstone/entities/Enrollments.java
+++ gcuCapstone/src/main/java/co/grandcircus/gcuCapstone/entities/Enrollments.java
@@ -23,4 +23,45 @@ public class Enrollments {
 	
 	public Enrollments() {}
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
+	public Course getCourse() {
+		return course;
+	}
+
+	public void setCourse(Course course) {
+		this.course = course;
+	}
+
+	public Double getGrade() {
+		return grade;
+	}
+
+	public void setGrade(Double grade) {
+		this.grade = grade;
+	}
+
+	public String getSemester() {
+		return semester;
+	}
+
+	public void setSemester(String semester) {
+		this.semester = semester;
+	}
+
+	
 }
